b82dd306d5b95c5b6bfd6de457c6ddd05c66449d
Vinicius Cunha <vinicius2010.cunha@hotmail.com>
2022-05-23 07:27:07 -0300

    add startup class

---
+++ src/TheDevSpaceWebApp/Startup.cs
@@ -0,0 +1,31 @@
+﻿namespace TheDevSpaceWebApp;
+
+public class Startup
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllersWithViews();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (!env.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Home/Error");
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.UseEndpoints(e =>
+        {
+            e.MapControllerRoute(name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+        });
+    }
+}
