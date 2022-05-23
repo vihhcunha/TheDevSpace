a5fd6c5fcfe5dbdc1f01f9bebe39fa25c72e8237
Vinicius Cunha <vinicius2010.cunha@hotmail.com>
2022-05-23 08:07:11 -0300

    fix program.cs parameter

--- src/TheDevSpaceWebApp/Program.cs
+++ src/TheDevSpaceWebApp/Program.cs
@@ -3,7 +3,7 @@ using TheDevSpaceWebApp;
 var builder = WebApplication.CreateBuilder(args);
 
 var startup = new Startup();
-startup.ConfigureServices(builder.Services);
+startup.ConfigureServices(builder.Services, builder.Configuration);
 
 var app = builder.Build();
 startup.Configure(app, builder.Environment);
