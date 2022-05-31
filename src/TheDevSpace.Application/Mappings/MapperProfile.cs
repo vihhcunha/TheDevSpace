253b4d351c021c3ef60fec493e9eca8b65475269
Vinicius Cunha <vinicius2010.cunha@hotmail.com>
2022-05-31 07:22:58 -0300

    create mapper profile

---
+++ src/TheDevSpace.Application/Mappings/MapperProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using TheDevSpace.Domain.Entities;
+
+namespace TheDevSpace.Application.Mappings;
+
+public class MapperProfile : Profile
+{
+    public MapperProfile()
+    {
+        CreateMap<Article, ArticleDto>();
+        CreateMap<ArticleStar, ArticleStarDto>();
+        CreateMap<User, UserDto>();
+        CreateMap<Writer, WriterDto>();
+    }
+}
