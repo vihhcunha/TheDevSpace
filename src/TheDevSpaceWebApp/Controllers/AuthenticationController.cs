784235b71107c09f72c5f105d399f48e7235fb00
Vinicius Cunha <vinicius2010.cunha@hotmail.com>
2022-06-02 09:21:02 -0300

    create layout and login view

---
+++ src/TheDevSpaceWebApp/Controllers/AuthenticationController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TheDevSpaceWebApp.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        [HttpGet("Auth/Login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
