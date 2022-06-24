dedb4bf4509d7f6c4e176debda194adeef6ea9b7
Vinicius Cunha <vinicius2010.cunha@hotmail.com>
2022-06-24 16:33:00 -0300

    add health check route

---
+++ src/TheDevSpaceWebApp/Controllers/HealthCheckController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TheDevSpaceWebApp.Controllers
+{
+    public class HealthCheckController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("ok");
+        }
+    }
+}
